99f28915cff5f55455e81e24b5e2e407a2edf25c
John Hawkins <john.hawkins@john.hawkins.MacBook>
2020-03-12 12:33:45 +1100

    Adding functioning timeseries version

--- scripts/DataRobot_timeseries_model_factory.R
+++ scripts/DataRobot_timeseries_model_factory.R
@@ -2,25 +2,51 @@ library(datarobot)
 library(data.table)
 
 # ####################################################################
-# Generate a range of models from the specified data
-# df 		- (Dataframe) A dataframe containing all data
-# date_col 	- (String) The time differntiaing column
-# targets 	- (Array) The list of target columns to build independent models
-# project_name 	- (String) Prefix for project name in datarobot
-# result_dir 	- (String) Path to write the results
-# metric 	- (String) The metric to target
-# other_metrics - (Array) Other metrics to report on for the results file
+# Generate a range of timeseries models from the specified data
+# df            - (Dataframe) A dataframe containing all data
+# date_col      - (String) The time differntiaing column
+# targets       - (Array) The list of target columns to build independent models
+# project_name  - (String) Prefix for project name in datarobot
+# result_dir    - (String) Path to write the results
+# metric        - (String) The metric to report on
+#
+# NOTE: There is no error checking on the parameters. 
+#       Any reference to columns that do not exist will result in a fatal error.
 # ####################################################################
 
-datarobot_timeseries_model_factory <- function(df, date_col targets, project_name, result_dir, metric, other_metrics) {
+# CHANGE THIS IF YOU ARE USING A LOCAL DATAROBOT INSTALL
+base_url="https://app.datarobot.com/"
+
+datarobot_timeseries_model_factory <- function(df, date_col, targets, project_name, result_dir, metric) {
 
 	# WE WRITE OUT THE MODEL RESULTS INTO A TABLE
-	resultsFile = paste(resultDir, 'model_list.tsv', sep='/')
+        rez_file_name = paste(project_name, "_model_list.tsv", sep='')
+        resultsFile = paste(result_dir, rez_file_name, sep='/')
 
-	# Test the output file before we begin 
+        page_file_name = paste(project_name, "_model_list.html", sep='')
+	resultsPage = paste(result_dir, page_file_name, sep='/')
+
+	# Write a header to the results files before we begin 
 	result = tryCatch({
 		rez <- file(resultsFile, "w")
-		writeLines("target\tdatarobot_project_id\tdatarobot_model_id\tmetric",con=rez,sep="\n")
+		writeLines( paste("target\tdatarobot_project_id\tdatarobot_model_id\t", metric, sep=""),con=rez,sep="\n")
+
+		page <- file(resultsPage, "w")
+		writeLines( 
+                   paste( "<html> <head> 
+                           <title>DataRobot Models for", project_name, "</title> 
+                           <link rel='stylesheet' href='style.css'>
+                           </head> <body>", sep=""), 
+                   con=page, sep="\n" 
+                )
+                header = paste("<div class='rTable'>
+                 <div class='rTableRow'>
+                 <div class='rTableHead'>Model</div>
+                 <div class='rTableHead'>Model Type</div>
+                 <div class='rTableHead'>", metric, "</div>
+                 </div>", sep="")
+                writeLines( header, con=page, sep="\n")
+
 	}, warning = function(w) {
     		message('Potential problem with writing your results file.')
 		message(w)
@@ -34,35 +60,50 @@ datarobot_timeseries_model_factory <- function(df, date_col targets, project_nam
 	})
 
 	# Force data frame to be a data table
-	dt	<- data.table(df)
-        colnames	<- names(dt)
-	featureList 	<- colnames[!colnames %in% targets] 
+	dt		<- data.table(df)
 
+        dt_partition = CreateDatetimePartitionSpecification(
+		datetimePartitionColumn = date_col,
+		useTimeSeries = TRUE
+ 	)
+	
 	# iterate over the keyset
 	for(target in targets) { 
 		# Subset the data and create the project for this key
 		temp.data	<- dt
-		projName 	<- paste(projectNamePrefix, target, sep='_')
+		projName 	<- paste(project_name, target, sep='_')
 		temp.proj	<- SetupProject( dataSource=temp.data, projectName=projName )
-		SetTarget(project=temp.proj, target=target, mode = 'manual')
-		
-		F_id = CreateFeaturelist(temp.proj$projectId, 'featureList', featureList)
-		StartNewAutoPilot(temp.proj, featurelistId = F_id)
 
+		#SetTarget(project=temp.proj, target=target, partition = dt_partition, mode = 'manual')
+		SetTarget(project=temp.proj, target=target, partition = dt_partition)
+		
+		#StartNewAutoPilot(temp.proj)
+
+                UpdateProject(project = temp.proj$projectId, workerCount = -1)
+
+                print("Auto-Pilot Started... Halting until completion")
 		WaitForAutopilot(project = temp.proj)
 
-		# Once Autopilot has finished we retrieve the best model ID
-		all.models 	<- ListModels(temp.proj)
-		model.frame 	<- as.data.frame(all.models)
-		model.type 	<- model.frame$modelType
-
-		best.model	<- all.models[[1]]
+		# Once Autopilot has finished we retrieve the most accurate model details 
+                best.model	<- GetRecommendedModel(temp.proj, type = RecommendedModelType$MostAccurate)
+		model.type 	<- best.model$modelType
 		modelId		<- best.model$modelId
 		metric		<- best.model$metrics[[metric]]$validation		
+                url 		<- paste( base_url, "projects/", temp.proj$projectId, "/models/", modelId, "/accuracy-over-time", sep="")
+                entry		<- paste("<div class='rTableRow'>
+                                          <div class='rTableCell'><a href='", url ,"'>", target, "</a></div>
+                                          <div class='rTableCell'>", model.type, "</div>
+                                          <div class='rTableCell'>", metric, "</div>
+                                          </div>", sep="")
 		writeLines(paste(target, temp.proj$projectId, modelId, metric, sep='\t'), con=rez, sep="\n")		
+                writeLines( entry, con=page, sep="\n")
 	}
+        # FINISH THE TABLE IN THE HTML OUTPUT
 
+        footer = paste("</body></html>")
+        writeLines( footer, con=page, sep="\n")
 	# CLOSE THE RESULTS FILE 
+	close(page)
 	close(rez)
 	return(1)
 } 
--- scripts/timeseries_example.R
+++ scripts/timeseries_example.R
@@ -1,4 +1,14 @@
-
 source("DataRobot_timeseries_model_factory.R")
+
+dataset = "../data/Sales.csv"
+
+df 		<- read.csv(dataset)
+targets 	<- c("product_A", "product_B") 
+date_field	<- "date"
+project_name 	<- "Sales_Models" 
+result_dir 	<- "results"
+metric 		<- "RMSE"
+
+datarobot_timeseries_model_factory(df, date_field, targets, project_name, result_dir, metric)
 
 
